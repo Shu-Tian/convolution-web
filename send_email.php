3330132df374851f1e258cbd597fefe0052ec1a4
Shu <47764958+Shu-Tian@users.noreply.github.com>
2025-02-01 00:29:23 -0500

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,17 @@
+<!DOCTYPE php>
+<html>
+<body>
+
+  <?php
+  // the message
+  $msg = "First line of text\nSecond line of text";
+
+  // use wordwrap() if lines are longer than 70 characters
+  $msg = wordwrap($msg,70);
+
+  // send email
+  mail("shuop@hotmail.com","My subject",$msg);
+  ?>
+
+</body>
+</html>
